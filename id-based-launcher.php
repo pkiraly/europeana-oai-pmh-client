2a5d4a4d7f8b024e58b0b28cb2db234a52494155
Péter Király <kirunews@gmail.com>
2015-12-16 17:58:01 +0100

    Fix launcher.

--- id-based-launcher.php
+++ id-based-launcher.php
@@ -14,10 +14,10 @@ if (filesize(SET_FILE_NAME) > 3) {
   echo 'count: ', count($lines), "\n";
   $line = array_shift($lines);
   $contents = join("\n", $lines);
-  file_put_contents('setlist.txt', $contents);
+  file_put_contents(SET_FILE_NAME, $contents);
   echo 'count: ', count($lines), "\n";
 
   list($set, $id) = explode("\t", $line);
-  printf("%s launching set: %s\n", date("Y-m-d H:i:s"), $set);
+  printf("%s launching set: %s, id: %s\n", date("Y-m-d H:i:s"), $set, $id);
   exec('nohup php id-based-harvester.php --set="' . $set . '" --id="' . $id . '">>id-based-report.txt 2>>id-based-report.txt &');
 }
