e681534ae481cfd53657c25a9c8e123edadf7209
Peter Kiraly <peter.kiraly@gwdg.de>
2015-12-16 17:33:34 +0100

    Change report name.

--- id-based-lancher.php
+++ id-based-lancher.php
@@ -17,5 +17,5 @@ if (filesize(SET_FILE_NAME) > 3) {
 
   list($set, $id) = explode("\t", $line);
   printf("%s launching set: %s\n", date("Y-m-d H:i:s"), $set);
-  exec('nohup php id-based-harvester.php --set="' . $set . '" --id="' . $id . '">>oai2json-report.txt 2>>oai2json-report.txt &');
+  exec('nohup php id-based-harvester.php --set="' . $set . '" --id="' . $id . '">>id-based-report.txt 2>>id-based-report.txt &');
 }
